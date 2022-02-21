74f9e3e7506145269a441ab922ff01f4a869fb38
Huynh Thanh Viet <huynhthviet@gmail.com>
2022-02-21 22:05:01 +0700

    format code

--- app/views.py
+++ app/views.py
@@ -1,10 +1,11 @@
 from flask import render_template, request
-from flask import redirect, url_for
 import os
 from PIL import Image
 from app.utils import pipeline_model
 
 UPLOAD_FLODER = 'static/uploads'
+
+
 def base():
     return render_template('base.html')
 
@@ -16,26 +17,26 @@ def index():
 def faceapp():
     return render_template('faceapp.html')
 
+
 def getwidth(path):
     img = Image.open(path)
-    size = img.size # width and height
-    aspect = size[0]/size[1] # width / height
+    size = img.size  # width and height
+    aspect = size[0]/size[1]  # width / height
     w = 300 * aspect
     return int(w)
+
 
 def gender():
 
     if request.method == "POST":
         f = request.files['image']
-        filename=  f.filename
-        path = os.path.join(UPLOAD_FLODER,filename)
+        filename = f.filename
+        path = os.path.join(UPLOAD_FLODER, filename)
         f.save(path)
         w = getwidth(path)
         # prediction (pass to pipeline model)
-        pipeline_model(path,filename,color='bgr')
+        pipeline_model(path, filename, color='bgr')
 
+        return render_template('gender.html', fileupload=True, img_name=filename, w=w)
 
-        return render_template('gender.html',fileupload=True,img_name=filename, w=w)
-
-
-    return render_template('gender.html',fileupload=False,img_name="freeai.png")
+    return render_template('gender.html', fileupload=False, img_name="freeai.png")
